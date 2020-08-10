fae98aad6f8bed4b8ab01bf6f15216b8259b326c
unknown <shivikasingla24@gmail.com>
2020-08-10 15:26:22 +0100

    shivika

---
+++ login/dbconnection.js
@@ -0,0 +1,15 @@
+const mysql = require('mysql');
+var con = mysql.createConnection({
+    host:"localhost",
+    user:"root",
+    password:"",
+    database: "login"
+});
+con.connect(function(err){
+    if(!err) {
+        console.log("Database is connected ... ");
+    } else {
+        console.log("Error connecting database ... ");
+    }
+    });
+module.exports = con; 
---
+++ login/server.js
@@ -0,0 +1,24 @@
+var express    =  require("express");
+
+var bodyParser = require('body-parser');
+var cors = require('cors');
+var app = express();
+require('./dbconnection');
+
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(function(req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin',"*")
+    res.setHeader("Access-Control-Allow-Headers","Content-Type,Acess-Control-Allow-Headers,Authorization,X-Requested-With");
+    res.setHeader("Access-Control-Allow-Methods","*");
+    next();
+});
+
+var router = express.Router();
+
+
+app.listen(8082,()=>{ 
+    console.log("Server is Listening At Port 8082")  
+})
